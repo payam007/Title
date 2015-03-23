952a3903c06ea0cd5bed43a28593ac9ca7a178a5
unknown <pk23217@an098082-ct107v.nvstu.edu>
2015-03-23 14:26:46 -0400

    add Title

---
+++ Ex3.5.java
@@ -0,0 +1,12 @@
+Import javax.swing.JOptionPane;
+public class Title{
+        public static viod main(String [] args){
+        String[] choices = {"Admin", "Studnet", "Staff", "Faculty", Gu
+        String input = (String) JOptionPane.showInputDialog(null, "Wha
+        position ","Account Type",JOptionPane.QUESTION_MESSAFE, null,c
+        ;
+        System.out.println("welcome " + input);
+        }
+
+        }
+
